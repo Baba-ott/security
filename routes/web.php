0d584e4b446944fa71cff1ac7d915c1ffed02768
Baba-ott <inig0001@hz.nl>
2023-06-16 14:33:31 +0200

    updates.3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('throttle:60,1');
 
 require __DIR__.'/auth.php';
